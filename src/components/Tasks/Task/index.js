9554bce89f69b6d3987dcb892d36a3340706bb71
Kaleb Jubar <k_jubar@fanshaweonline.ca>
2024-07-03 14:48:40 -0400

    Move task details and additional buttons to modal

--- src/components/Tasks/Task/index.js
+++ src/components/Tasks/Task/index.js
@@ -1,16 +1,23 @@
 /*
  *  Author: Kaleb Jubar
  *  Created: 29 May 2024, 5:26:51 PM
- *  Last update: 5 Jun 2024, 12:48:55 PM
+ *  Last update: 3 Jul 2024, 2:35:08 PM
  *  Copyright (c) 2024 Kaleb Jubar
  */
-import { View, Text, Switch, Pressable, TouchableHighlight } from "react-native";
+import {
+    View, Text, Switch, Pressable,
+    TouchableHighlight, Modal, TouchableOpacity
+} from "react-native";
 import { Ionicons } from '@expo/vector-icons';
+import { MaterialIcons } from '@expo/vector-icons';
 
 import styles from "./styles";
 import { primaryColor } from "../../../includes/variables";
+import { useState } from "react";
 
 export default function Task({ id, description, completed, setCompleted, deleteTask }) {
+    const [showDetailModal, setShowDetailModal] = useState(false);
+
     /**
      * Toggle the completed status of this task.
      */
@@ -18,32 +25,75 @@ export default function Task({ id, description, completed, setCompleted, deleteT
         setCompleted(id, !completed);
     };
 
+    /**
+     * Toggle whether the modal is shown
+     */
+    const toggleModal = () => {
+        setShowDetailModal(!showDetailModal);
+    };
+
+    /**
+     * Delete this task from the list
+     */
+    const deleteThisTask = () => {
+        deleteTask(id);
+    };
+
     return (
-        <View style={styles.container}>
-            <Text style={styles.title}>{description}</Text>
-            <View style={styles.switch}>
-                {/* like on the form, pressable around the label to toggle switch */}
-                <Pressable onPress={toggleCompleted}>
-                    <Text style={styles.switchText}>Completed?</Text>
+        <>
+            <Pressable style={styles.container} onPress={toggleModal}>
+                <Text style={styles.title}>{description}</Text>
+                <Text style={styles.info}>Completed? {completed ? "Yes" : "No"}</Text>
+                <Text style={styles.info}>ID: {id}</Text>
+            </Pressable>
+
+            <Modal visible={showDetailModal} animationType="fade" transparent={true}>
+                {/* make the container pressable so you can click outside the
+                    modal to close it */}
+                <Pressable style={styles.modalContainer} onPress={toggleModal}>
+                    {/* this also needs to be a pressable so we can cancel
+                        bubbling of events to the parent pressable
+                        this makes the modal only close when clicking the overlay,
+                        not the modal itself */}
+                    <Pressable style={styles.modal} onPress={(e) => e.preventDefault}>
+                        <Text style={styles.title}>{description}</Text>
+                        
+                        <View style={styles.actions}>
+                            <View style={styles.switch}>
+                                {/* like on the form, pressable around the label to toggle switch */}
+                                <Pressable onPress={toggleCompleted}>
+                                    <Text style={styles.switchText}>Completed?</Text>
+                                </Pressable>
+                                <Switch
+                                    style={styles.switchSizing}
+                                    value={completed}
+                                    onValueChange={toggleCompleted}
+                                    trackColor={{ false: "#DDD", true: primaryColor }}
+                                    ios_backgroundColor="#DDD"
+                                    thumbColor="white"
+                                />
+                            </View>
+
+                            <TouchableOpacity
+                                style={styles.deleteButton}
+                                onPress={deleteThisTask}
+                            >
+                                <MaterialIcons name="delete-outline" size={24} color="white" />
+                                <Text style={styles.deleteButton.text}>Delete</Text>
+                            </TouchableOpacity>
+                        </View>
+
+                        {/* close button, placed using position: absolute */}
+                        <TouchableHighlight
+                            style={styles.closeButton}
+                            onPress={toggleModal}
+                            underlayColor="#CCC"
+                        >
+                            <Ionicons name="close" size={24} color="black" />
+                        </TouchableHighlight>
+                    </Pressable>
                 </Pressable>
-                <Switch
-                    style={styles.switchSizing}
-                    value={completed}
-                    onValueChange={toggleCompleted}
-                    trackColor={{ false: "#DDD", true: primaryColor }}
-                    ios_backgroundColor="#DDD"
-                    thumbColor="white"
-                />
-            </View>
-            <Text style={styles.info}>ID: {id}</Text>
-            {/* delete button, placed using position: absolute */}
-            <TouchableHighlight
-                style={styles.closeButton}
-                onPress={() => deleteTask(id)}
-                underlayColor="#CCC"
-            >
-                <Ionicons name="close" size={24} color="black" />
-            </TouchableHighlight>
-        </View>
+            </Modal>
+        </>
     );
 }
--- src/components/Tasks/Task/styles.js
+++ src/components/Tasks/Task/styles.js
@@ -1,7 +1,7 @@
 /*
  *  Author: Kaleb Jubar
  *  Created: 29 May 2024, 6:33:29 PM
- *  Last update: 3 Jun 2024, 6:34:31 PM
+ *  Last update: 3 Jul 2024, 2:42:04 PM
  *  Copyright (c) 2024 Kaleb Jubar
  */
 import { Platform, StyleSheet } from "react-native";
@@ -41,6 +41,8 @@ export default StyleSheet.create({
     title: {
         fontWeight: "bold",
         fontSize: 17,
+        marginBottom: 5,
+        marginRight: 20,
     },
     
     switch: {
@@ -60,5 +62,52 @@ export default StyleSheet.create({
         color: secondaryColor,
         opacity: 0.7,
         fontSize: 12,
+    },
+
+    actions: {
+        flexDirection: "row",
+        justifyContent: "space-between",
+        marginTop: 10,
+    },
+
+    deleteButton: {
+        backgroundColor: "red",
+        flexDirection: "row",
+        alignItems: "center",
+        padding: 5,
+        marginVertical: 4,
+        paddingRight: 7,
+        borderRadius: 5,
+        gap: 5,
+
+        text: {
+            color: "white",
+        },
+    },
+
+    modalContainer: {
+        flex: 1,
+        alignItems: "center",
+        justifyContent: "center",
+        backgroundColor: "rgba(0, 0, 0, 0.25)",
+    },
+
+    modal: {
+        backgroundColor: "white",
+        padding: 10,
+        width: "70%",
+        borderRadius: 10,
+
+        // Android
+        elevation: 5,
+
+        // iOS
+        shadowOpacity: 0.25,
+        shadowRadius: 4,
+        shadowColor: "#000", // also applies to Android
+        shadowOffset: {
+            width: 0,
+            height: 4,
+        },
     },
 });
