b8a90f4efdac5f4cfa4162fd17ce9e8c34788e21
Kaleb Jubar <k_jubar@fanshaweonline.ca>
2024-07-03 13:09:09 -0400

    Fix tab bar spacing

--- App.js
+++ App.js
@@ -1,7 +1,7 @@
 /*
  *  Author: Kaleb Jubar
  *  Created: 26 Oct 1985, 4:15:00 AM
- *  Last update: 3 Jul 2024, 12:53:24 PM
+ *  Last update: 3 Jul 2024, 12:55:37 PM
  *  Copyright (c) 1985 - 2024 Kaleb Jubar
  */
 import { StatusBar } from "expo-status-bar";
@@ -102,6 +102,9 @@ export default function App() {
 
                 <Tab.Navigator screenOptions={{
                     tabBarActiveTintColor: primaryColor,
+                    tabBarStyle: {
+                        marginBottom: 7,
+                    },
                 }}>
                     <Tab.Screen
                         name="TasksScreen"
