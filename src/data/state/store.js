dcd99bdd135ccad6bf3bf6584f74bfe16d9b9095
Kaleb Jubar <k_jubar@fanshaweonline.ca>
2024-07-15 10:12:43 -0400

    Add redux

---
+++ src/data/state/store.js
@@ -0,0 +1,13 @@
+/*
+ *  Author: Kaleb Jubar
+ *  Created: 15 Jul 2024, 10:12:04 AM
+ *  Last update: 
+ *  Copyright (c) 2024 Kaleb Jubar
+ */
+import { configureStore } from "@reduxjs/toolkit";
+
+export const store = configureStore({
+    reducer: {
+
+    },
+});
